fa52253a47ae0e796d938b5dc6a9d464befe47fe
pjiaquan <pjiaquan@gmail.com>
2025-12-06 17:15:25 +0800

    release apk

--- app/src/main/java/my/hinoki/booxreader/data/ui/notes/AiNoteDetailActivity.kt
+++ app/src/main/java/my/hinoki/booxreader/data/ui/notes/AiNoteDetailActivity.kt
@@ -301,6 +301,10 @@ class AiNoteDetailActivity : AppCompatActivity() {
             binding.scrollView.post {
                 checkScrollPosition()
             }
+        } else if (previousScrollY != null) {
+            binding.scrollView.post {
+                binding.scrollView.scrollTo(0, previousScrollY)
+            }
         }
     }
 
