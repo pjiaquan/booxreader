50d4339149ec63e5bc58b7f5596111577d6286b8
pjiaquan <support@hinoki.my>
2025-12-14 17:53:13 +0800

    feat: Update app/src/main/res/drawable/ic_person.xml app/src/main/res/layout/activity_main.xml app/src/main/res/layout/activity_reader.xml (v1.1.58)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,8 +68,8 @@ android {
         applicationId = "my.hinoki.booxreader"
         minSdk = 24
         targetSdk = 35
-        versionCode = 57
-        versionName = "1.1.56"
+        versionCode = 59
+        versionName = "1.1.58"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
