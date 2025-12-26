2085d1441792072148176da6c6b5af8ac18ca890
pjiaquan <support@hinoki.my>
2025-12-26 11:13:25 +0800

    Bump version to 1.1.97

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,8 +79,8 @@ android {
         applicationId = "my.hinoki.booxreader"
         minSdk = 24
         targetSdk = 35
-        versionCode = 97
-        versionName = "1.1.96"
+        versionCode = 98
+        versionName = "1.1.97"
 
         buildConfigField("String", "SUPABASE_URL", "\"$supabaseUrl\"")
         buildConfigField("String", "SUPABASE_ANON_KEY", "\"$supabaseAnonKey\"")
