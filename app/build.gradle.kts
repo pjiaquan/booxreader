308358169bc7e6fc310482deeac1c9886ba337a2
Jiaquan <pjiaquan@gmail.com>
2026-03-27 08:55:58 +0800

    chore: update application versionCode to 243 and versionName to 1.1.242

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,8 +91,8 @@ android {
         applicationId = "my.hinoki.booxreader"
         minSdk = 24
         targetSdk = 35
-        versionCode = 240
-        versionName = "1.1.239"
+        versionCode = 243
+        versionName = "1.1.242"
 
         buildConfigField("String", "POCKETBASE_URL", "\"$pocketbaseUrl\"")
         buildConfigField("String", "R2_ENDPOINT", "\"$r2Endpoint\"")
