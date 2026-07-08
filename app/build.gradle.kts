bd1ba7fbee65f32c2e13aa5d2f5e6e6bdb1eea41
Jiaquan <pjiaquan@gmail.com>
2026-07-08 22:34:55 +0800

    chore(release): bump versionCode to 254 and versionName to 1.1.253
    * Increment versionCode from 253 to 254
    * Update versionName from "1.1.252" to "1.1.253

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,8 +91,8 @@ android {
         applicationId = "my.hinoki.booxreader"
         minSdk = 24
         targetSdk = 35
-        versionCode = 253
-        versionName = "1.1.252"
+        versionCode = 254
+        versionName = "1.1.253"
 
         buildConfigField("String", "POCKETBASE_URL", "\"$pocketbaseUrl\"")
         buildConfigField("String", "R2_ENDPOINT", "\"$r2Endpoint\"")
