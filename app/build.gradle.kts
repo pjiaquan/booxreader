92eb18e2cc23869369ed355e330364e1db8f3b31
Jiaquan <pjiaquan@gmail.com>
2026-07-26 18:23:21 +0800

    fix: update .github/workflows/android_ui_test.yml (v1.1.264)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,8 +91,8 @@ android {
         applicationId = "my.hinoki.booxreader"
         minSdk = 24
         targetSdk = 35
-        versionCode = 264
-        versionName = "1.1.263"
+        versionCode = 265
+        versionName = "1.1.264"
 
         buildConfigField("String", "POCKETBASE_URL", "\"$pocketbaseUrl\"")
         buildConfigField("String", "R2_ENDPOINT", "\"$r2Endpoint\"")
