84adb4879d4f64c9577f6f4aaff2ffd668ef3d29
Jiaquan <pjiaquan@gmail.com>
2026-07-26 18:00:37 +0800

    chore: update app behavior (4 files changed) (v1.1.258)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,8 +91,8 @@ android {
         applicationId = "my.hinoki.booxreader"
         minSdk = 24
         targetSdk = 35
-        versionCode = 258
-        versionName = "1.1.257"
+        versionCode = 259
+        versionName = "1.1.258"
 
         buildConfigField("String", "POCKETBASE_URL", "\"$pocketbaseUrl\"")
         buildConfigField("String", "R2_ENDPOINT", "\"$r2Endpoint\"")
